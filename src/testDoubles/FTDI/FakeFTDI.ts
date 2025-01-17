21b802851eee652bbc733785f4d49cc2c9398e92
ericthecurious <hello@ericthecurious.com>
2025-01-16 23:55:48 -0700

    patch: create static fakeDevices

--- src/testDoubles/FTDI/FakeFTDI.ts
+++ src/testDoubles/FTDI/FakeFTDI.ts
@@ -3,9 +3,11 @@ import { FTDI_Device } from 'ftdi-d2xx'
 export default class FakeFTDI {
     public static numCallsToGetDeviceInfoList = 0
 
+    public static fakeDevices: FTDI_Device[] = []
+
     public static async getDeviceInfoList() {
         FakeFTDI.numCallsToGetDeviceInfoList++
-        return [] as FTDI_Device[]
+        return this.fakeDevices
     }
 
     public static resetTestDouble() {
