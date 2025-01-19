9651062e72687e934a637198fc2c1dd51ae0f9f6
ericthecurious <hello@ericthecurious.com>
2025-01-18 18:12:44 -0700

    patch: extract and refactor

--- src/components/Cgx/CgxStreamProducer.ts
+++ src/components/Cgx/CgxStreamProducer.ts
@@ -7,6 +7,7 @@ export default class CgxStreamProducer implements LslProducer {
     public static FTDI = FTDI
 
     public isRunning = false
+    private infos!: FTDI.FTDI_DeviceInfo[]
     private device!: FTDI.FTDI_Device
 
     protected constructor() {}
@@ -20,32 +21,87 @@ export default class CgxStreamProducer implements LslProducer {
     }
 
     private async connectFtdi() {
-        const infos = await this.FTDI.getDeviceInfoList()
+        await this.loadFtdiDeviceInfos()
+        await this.openDeviceBySerialNumber()
 
-        if (infos.length === 0) {
+        this.configureDevice()
+    }
+
+    private async loadFtdiDeviceInfos() {
+        this.infos = await this.FTDI.getDeviceInfoList()
+        this.throwIfDeviceNotFound()
+    }
+
+    private throwIfDeviceNotFound() {
+        if (this.infos.length === 0) {
             throw new SpruceError({ code: 'CGX_FTDI_DEVICE_NOT_FOUND' })
         }
+    }
 
-        const serialNumber = infos[0].serial_number
-        this.device = await this.FTDI.openDevice(serialNumber)
+    private async openDeviceBySerialNumber() {
+        this.device = await this.FTDI.openDevice(this.serialNumber)
+    }
 
-        this.device.setTimeouts(1000, 1000)
-        this.device.purge(FTDI.FT_PURGE_RX)
-        this.device.setFlowControl(FTDI.FT_FLOW_RTS_CTS, 0x11, 0x13)
-        this.device.setBaudRate(1000000)
+    private get serialNumber() {
+        return this.infos[0].serial_number
+    }
 
-        this.device.setDataCharacteristics(
-            FTDI.FT_BITS_8,
-            FTDI.FT_STOP_BITS_1,
-            FTDI.FT_PARITY_NONE
+    private configureDevice() {
+        this.setReadAndWriteTimeouts()
+        this.purgeReadBuffer()
+        this.setFlowControl()
+        this.setBaudRate()
+        this.setDataCharacteristics()
+        this.setLatencyTimer()
+    }
+
+    private setReadAndWriteTimeouts() {
+        this.device.setTimeouts(this.readTimeoutMs, this.writeTimeoutMs)
+    }
+
+    private purgeReadBuffer() {
+        this.device.purge(this.ftdiReadBuffer)
+    }
+
+    private setFlowControl() {
+        this.device.setFlowControl(
+            this.ftdiFlowControlMode,
+            this.seventeenInHex,
+            this.nineteenInHex
         )
+    }
 
-        this.device.setLatencyTimer(4)
+    private setBaudRate() {
+        this.device.setBaudRate(this.baudRate)
+    }
+
+    private setDataCharacteristics() {
+        this.device.setDataCharacteristics(
+            this.ftdiDataBits,
+            this.ftdiStopBits,
+            this.noParityBit
+        )
+    }
+
+    private setLatencyTimer() {
+        this.device.setLatencyTimer(this.latencyTimerMs)
     }
 
     public async stopLslStreams() {}
 
     public async disconnect() {}
+
+    private readonly readTimeoutMs = 1000
+    private readonly writeTimeoutMs = 1000
+    private readonly ftdiReadBuffer = FTDI.FT_PURGE_RX
+    private readonly ftdiFlowControlMode = FTDI.FT_FLOW_RTS_CTS
+    private readonly seventeenInHex = 0x11
+    private readonly nineteenInHex = 0x13
+    private readonly baudRate = 1000000
+    private readonly ftdiDataBits = FTDI.FT_BITS_8
+    private readonly ftdiStopBits = FTDI.FT_STOP_BITS_1
+    private readonly noParityBit = FTDI.FT_PARITY_NONE
+    private readonly latencyTimerMs = 4
 
     private get FTDI() {
         return CgxStreamProducer.FTDI
