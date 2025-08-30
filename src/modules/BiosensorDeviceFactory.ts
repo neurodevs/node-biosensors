dfb515c329a3daa24d7472190dec90b0d5a40794
ericthecurious <hello@ericthecurious.com>
2025-08-30 16:53:09 -0700

    patch: add protected constructor

--- src/modules/BiosensorDeviceFactory.ts
+++ src/modules/BiosensorDeviceFactory.ts
@@ -1,6 +1,8 @@
 export default class BiosensorDeviceFactory {
     public static Class?: DeviceFactoryConstructor
 
+    protected constructor() {}
+
     public static Create() {
         return new (this.Class ?? this)()
     }
