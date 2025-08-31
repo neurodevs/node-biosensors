e86f3c19474adcc449b49d4c3bdac0520aa3daf8
ericthecurious <hello@ericthecurious.com>
2025-08-30 18:07:31 -0700

    minor: create demo script for factory

---
+++ src/scripts/runFactory.ts
@@ -0,0 +1,10 @@
+import BiosensorDeviceFactory from '../modules/BiosensorDeviceFactory'
+
+async function main() {
+    const factory = BiosensorDeviceFactory.Create()
+    await factory.createDevice('Cognionics Quick-20r')
+}
+
+main().catch((error) => {
+    console.error('Error in main:', error)
+})
