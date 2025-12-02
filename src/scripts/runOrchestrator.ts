41485d6ce07869941c03f6e00a5e26b0344fad15
ericthecurious <hello@ericthecurious.com>
2025-12-01 22:23:16 -0800

    minor: create script to run orchestrator

---
+++ src/scripts/runOrchestrator.ts
@@ -0,0 +1,18 @@
+import BiosensorRuntimeOrchestrator from '../impl/BiosensorRuntimeOrchestrator.js'
+
+const orchestrator = await BiosensorRuntimeOrchestrator.Create({
+    deviceNames: ['Muse S Gen 2'],
+    xdfRecordPath: './src/__tests__/test.xdf',
+})
+
+await orchestrator.start()
+
+console.log('Biosensor runtime orchestrator started successfully.')
+
+await new Promise((resolve) => {
+    setTimeout(resolve, 60 * 1000)
+})
+
+await orchestrator.stop()
+
+console.log('Biosensor runtime orchestrator stopped successfully.')
