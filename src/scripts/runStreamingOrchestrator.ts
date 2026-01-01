50d79cef5b2ba8aff71e061d3e731f6c66837acf
ericthecurious <hello@ericthecurious.com>
2025-12-31 20:23:39 -0800

    patch: fix domain name

--- src/scripts/runStreamingOrchestrator.ts
+++ src/scripts/runStreamingOrchestrator.ts
@@ -1,19 +1,19 @@
 import BiosensorStreamingOrchestrator from '../impl/BiosensorStreamingOrchestrator.js'
 
-const server = await BiosensorStreamingOrchestrator.Create({
+const orchestrator = await BiosensorStreamingOrchestrator.Create({
     deviceNames: ['Muse S Gen 2'],
     // xdfRecordPath: './src/__tests__/test.xdf',
     webSocketPortStart: 8080,
 })
 
-await server.start()
+await orchestrator.start()
 
-console.log('Biosensor streaming server started successfully.')
+console.log('Biosensor streaming orchestrator started successfully.')
 
 await new Promise((resolve) => {
     setTimeout(resolve, 5 * 1000)
 })
 
-await server.stop()
+await orchestrator.stop()
 
-console.log('Biosensor streaming server stopped successfully.')
+console.log('Biosensor streaming orchestrator stopped successfully.')
