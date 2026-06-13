85b2e65a009406ea479d19598891ef222f11c611
ericthecurious <hello@ericthecurious.com>
2026-06-13 16:49:11 -0700

    patch: update txtRecordPath

--- src/scripts/runMuse.ts
+++ src/scripts/runMuse.ts
@@ -3,7 +3,7 @@ import MuseDeviceController from '../impl/devices/MuseDeviceController.js'
 const muse = await MuseDeviceController.Create({
     bleUuid: 'CA6A61B7-B7A8-AF24-3C9E-04A6A5012554',
     rssiIntervalMs: 1000,
-    txtRecordPath: 'muse_data.txt',
+    txtRecordPath: './artifacts/muse_data.txt',
     enableLogs: false,
     disableEeg: false,
     disablePpg: false,
