1b4ee32d2531020d565f55c39dd90b05615addfc
ericthecurious <hello@ericthecurious.com>
2026-06-15 19:42:10 -0700

    patch: show eventMarkers option

--- src/scripts/runStreamingOrchestrator.ts
+++ src/scripts/runStreamingOrchestrator.ts
@@ -4,6 +4,10 @@ const orchestrator = await BiosensorStreamingOrchestrator.Create({
     deviceNames: ['Muse S Gen 2'],
     xdfRecordPath: './artifacts/test.xdf',
     webSocketPortStart: 8080,
+    eventMarkers: [
+        { name: 'test-marker-1', waitAfterMs: 100 },
+        { name: 'test-marker-2', waitAfterMs: 100 },
+    ],
 })
 
 await orchestrator.start()
