d62a4ec07952c18dd6a87c398ce2b7624d8ce562
eric-yates <hello@ericthecurious.com>
2026-08-16 22:16:37 -0700

    patch: change default temperatureUnits to fahrenheit in script

--- src/scripts/runGovee.ts
+++ src/scripts/runGovee.ts
@@ -2,7 +2,7 @@ import GoveeDeviceController from '../impl/govee/GoveeDeviceController.js'
 
 const govee = await GoveeDeviceController.Create({
     deviceUuid: '179F4A82-A2DF-C241-DB2A-1DF990779106',
-    temperatureUnits: 'Kelvin',
+    temperatureUnits: 'Fahrenheit',
     txtRecordPath: './artifacts/govee.txt',
 })
 
